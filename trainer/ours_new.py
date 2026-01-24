75dce6d7bb5668cb86e3b0b9c09b75b5a76eb1d6
xuyu0010 <xuyu0014@e.ntu.edu.sg>
2026-01-24 22:13:01 +0800

    ReSet

--- trainer/ours_new.py
+++ trainer/ours_new.py
@@ -294,7 +294,7 @@ def ours_new(args, teacher_backbone, teacher_classifier, student_backbone, stude
 
 
         if args.Reset_student:
-            if abs(confidence_gate_prev - confidence_gate) < 1e-3 and confidence_gate < 0.4 * float(torch.log(torch.tensor(args.nb_cl)).item()):
+            if abs(confidence_gate_prev - confidence_gate) < 1e-3 and confidence_gate <= 0.4 * float(torch.log(torch.tensor(args.nb_cl)).item()):
                 print("Reset student model.")
                 student_backbone.load_state_dict(teacher_backbone.state_dict())
                 student_classifier.load_state_dict(teacher_classifier.state_dict())
