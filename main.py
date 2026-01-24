a093d0a5eb29c0a0020f1e3ad276424d1fa43698
BJChen <chenbojian1997@gmail.com>
2026-01-24 22:13:46 +0800

    1

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ parser.add_argument('--lr', default=0.1, type=float, help='the learning rate')
 ### Dataset parameters
 parser.add_argument('--batch_size', default=64, type=int, help='the batch size for data loader, for iFlytek, it is ·64')
 parser.add_argument('--test_batch_size', default=100, type=int, help='the batch size for test data loader')
-parser.add_argument('--dataset_name', default='iFlytek', type=str, choices=['SK', 'SK_new', 'iFlytek', 'WT', 'PU_Real', 'PU_Art'], help='the dataset name')
+parser.add_argument('--dataset_name', default='SK', type=str, choices=['SK', 'SK_new', 'iFlytek', 'WT', 'PU_Real', 'PU_Art'], help='the dataset name')
 parser.add_argument('--data_num_class', default=100, type=int, help='the number of data in each class')
 
 ### No need to set the following parameters 
@@ -62,8 +62,7 @@ parser.add_argument('--SR', action='store_false', help='Enable basic SR')
 parser.add_argument('--MI', action='store_false', help='Enable MI loss')
 parser.add_argument('--EMA', action='store_false', help='Enable EMA for Mean-Teacher')
 parser.add_argument('--Reset_student', action='store_false', help='Reset student from teacher each epoch')
-parser.add_argument('--TOPK', action='store_false', help='the ablation setting')
-parser.add_argument('--PCA', action='store_false', help='the ablation setting')
+
 
 
 ### Hyperparameters for ours_new
